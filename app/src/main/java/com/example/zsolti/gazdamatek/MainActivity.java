b0b653867bae0f5380c4274fc0775208f17936ae
Zsolti <nagy.zsolt.1989@gmail.com>
2016-11-30 00:00:40 +0100

    Redesign CreateParcel Form into EditLand dialog

--- app/src/main/java/com/example/zsolti/gazdamatek/MainActivity.java
+++ app/src/main/java/com/example/zsolti/gazdamatek/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.zsolti.gazdamatek;
 
-import android.app.ListActivity;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
+
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import java.util.ArrayList;
 
 
-public class MainActivity extends ListActivity{
+public class MainActivity extends AppCompatActivity implements OnLandChangedListener
+{
 
     //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
     static ArrayList<Parcel> parcelListItems = new ArrayList<Parcel>();;
@@ -28,18 +35,9 @@ public class MainActivity extends ListActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //parcelListItems.add(new Parcel("Elso", "nagy", "0.0"));
-
-        Intent intent = getIntent();
-        if(getIntent().hasExtra("parcel name")) {
-            String temp1 = intent.getStringExtra("parcel name");
-            String temp2 = intent.getStringExtra("parcel area");
-            String temp3 = intent.getStringExtra("parcel plant");
-
-            Parcel tempparcel = new Parcel(temp1, temp2, temp3);
-            parcelListItems.add(tempparcel);
-        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if(parcelListItems.size() > 0) {
             listView = (ListView) findViewById( android.R.id.list );
@@ -66,6 +64,11 @@ public class MainActivity extends ListActivity{
         }
 
 */
+
+        //MAIN FLOATING ACTION BUTTON
+
+        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.main_fab);
+
         //ADD NEW PARCEL SUB BUTTON
 
         FloatingActionButton actionAddLand = new FloatingActionButton(getBaseContext());
@@ -78,8 +81,9 @@ public class MainActivity extends ListActivity{
         {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,CreateParcel.class);
-                startActivityForResult(i, 1);
+                menuMultipleActions.collapse();
+                EditLand editland = EditLand.newInstance();
+                editland.show(getFragmentManager(), "Edit land");
             }
         });
 
@@ -95,7 +99,7 @@ public class MainActivity extends ListActivity{
         {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Add new Income button clicked", Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -110,13 +114,11 @@ public class MainActivity extends ListActivity{
         actionAddOutcome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Add new Outcome button clicked", Toast.LENGTH_SHORT).show();
+
             }
         });
 
-        //MAIN FLOATING ACTION BUTTON
-
-        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.main_fab);
+        //ADD SUB BUTTONS TO MAIN MAIN FLOATING ACTION BUTTON
 
         menuMultipleActions.addButton(actionAddLand);
         menuMultipleActions.addButton(actionAddIncome);
@@ -163,21 +165,16 @@ public class MainActivity extends ListActivity{
 
         return super.onOptionsItemSelected(item);
     }
-/*
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode) {
-            case (1) : {
-                if (resultCode == MainActivity.RESULT_OK) {
-                    // TODO Extract the data returned from the child Activity.
-                }
-                break;
-            }
-        }
-    }
-*/
+
     public static ArrayList getParcelListItems(){
         return parcelListItems;
+    }
+
+    //RECREATE ACTIVITY WHEN DATA IS CHANGED
+    @Override
+    public void changeLandParameters(String name, String area, String plant) {
+        Parcel tempparcel = new Parcel(name, area, plant);
+        parcelListItems.add(tempparcel);
+        this.recreate();
     }
 }
